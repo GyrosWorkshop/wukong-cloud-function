5740d26c058110639642bd7d2663c6463bc1e50c
Richard <richard9372@gmail.com>
2018-07-10 11:52:55 +0800

    Deflate mq content

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var secret = require('./key.json')
 var request = require('request')
 var Capi = require('qcloudapi-sdk')
+var zlib = require('zlib')
 
 var capi = new Capi({
     SecretId: secret.secretId,
@@ -15,23 +16,23 @@ exports.main_handler = (event, context, callback) => {
     console.log(options)
     request(options.msg, (err, res) => {
         console.error(err)
-        try {
+        const msgBody = JSON.stringify({
+            key: options.key,
+            content: res,
+            error: err,
+        })
+        const buf = new Buffer(msgBody, 'utf8')
+        zlib.deflate(buf, (err, buf) => {
+            console.error(err)
             capi.request({
                 Action: 'PublishMessage',
                 topicName: 'wukong-callback',
-                msgBody: JSON.stringify({
-                    key: options.key,
-                    content: res,
-                    error: err
-                }),
+                msgBody: buf.toString('base64'),
             }, (err, data) => {
                 console.error(err)
                 console.log(data)
                 callback(err, event)
             })
-        } catch (e) {
-            console.error(e)
-            callback(e)
-        }
+        })
     })
 }
